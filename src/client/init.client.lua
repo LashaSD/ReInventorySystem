861f2a7294cb0920527b4fd648df3a5d3d4a5c96
LashaSD <trachenmacher@gmail.com>
2023-04-17 03:22:13 +0400

    implement: Basic Inv Data Retriever

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -1,6 +1,23 @@
--- print("Hello world, from client!")
+--- Services
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-local Player = game.Players.LocalPlayer
+--- Libs
+local InventoryMod = require(ReplicatedStorage.Common.InventoryHandler)
+
+--- Directories 
+local Events = ReplicatedStorage.Common.Events
+
+
+local Player = Players.LocalPlayer
 local Character = Player.Character or Player.CharacterAdded:Wait()
 
-print("Character Loaded ".. Character.Name)
+
+local ServerInventory = Events.GetStorageData:InvokeServer()
+local PlayerInventory = InventoryMod.new()
+PlayerInventory.StorageQueue = ServerInventory.StorageQueue
+
+if PlayerInventory then
+	print(PlayerInventory)
+	PlayerInventory:GenerateStoragesQueue()
+end 
